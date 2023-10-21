59779be26f0a39a8d1520abb1f303f2fb48761d5
bak72019 <143014417+bak72019@users.noreply.github.com>
2023-10-20 22:07:00 -0500

    Add files via upload

--- mp4/hdl/mem_stage.sv
+++ mp4/hdl/mem_stage.sv
@@ -2,19 +2,27 @@ module mem_stage
 import rv32i_types::*;
 import cpuIO::*;
 (
-    input clk,
-    input rst, 
-    input logic [31:0] alu_out,
-    input logic [31:0] rs2_out,
-    input cw_mem ctrl_w_EX,
-    input logic [31:0] pc_x,
-    input logic load_data_out,
-    input logic load_mar,
-    output logic [31:0] mem_address,
-    output logic [31:0] mem_wdata
+    input clk, //from datapath
+    input rst, //from datapath
+    input logic [31:0] alu_out,//from EXE_MEM pipeline reg
+    input logic [31:0] rs2_out,//from EXE_MEM pipeline reg
+    input cw_mem ctrl_w_EX,//from EXE_MEM pipeline reg
+    input logic [31:0] pc_x,//from EXE_MEM pipeline reg
+    input logic load_data_out, //from ??? would this always be 1?
+    input logic load_mar, //from ??? would this always be 1?
+    input rv32i_opcode opcode,//from EXE_MEM pipeline reg
+    input logic mem_resp_d //from data_cache
+    output logic [31:0] mem_address_d, //to data cache
+    output logic [31:0] mem_wdata_d, //to data cache
+    output logic mem_r_d, //to data cache
+    output logic mem_w_d, //to data cache
+    output logic mem_r_d_resp, //to MEM_WB pipeline reg
+    // output logic mem_stage_rdy //to exe_stage mem stage isn't busy if high, this will be a major bottle neck when low
 );
     logic [31:0] marmux_o;
-    logic marmux_sel;
+    // logic mem_stage_rdy_temp;
+
+    // assign mem_stage_rdy = mem_stage_rdy_temp;
 
     always_comb begin : mem_mux
         unique case (ctrl_w_EX.mar_sel)
@@ -23,8 +31,41 @@ import cpuIO::*;
         endcase
     end
 
-    always_comb begin : mem_ctrl
-        
+    function void do_default();
+        mem_r_d_resp <= 1'b0;
+        mem_r_d <= 1'b0;
+        mem_w_d <= 1'b0;
+        // mem_stage_rdy_temp <= 1'b1;
+    endfunction
+
+    always_ff @ (posedge clk, posedge rst) begin : mem_ctrl
+        if(rst) begin
+            do_default();
+        end
+        else begin
+            do_default();
+            case(opcode)
+                op_store: begin //mem_write to d cache until respond
+                    if (mem_resp_d == 1)begin 
+                        mem_r_d_resp <= 1'b1;
+                    end
+                    else begin
+                        mem_w_d <= 1'b1;
+                        // mem_stage_rdy_temp <= 1'b0;
+                    end
+                end
+                op_load: begin
+                    if(mem_resp_d == 1)begin //mem_read to d cache until respond
+                        mem_r_d_resp <= 1'b1;
+                    end
+                    else begin
+                        mem_r_d <= 1'b1;
+                        // mem_stage_rdy_temp <= 1'b0;
+                    end
+                end
+                default: ;
+            endcase
+        end
     end
 
     //we could include these registers in the pipeline 
@@ -32,17 +73,17 @@ import cpuIO::*;
     mem_data_out mdo_reg(
         .clk(clk),
         .reset(rst),
-        .load_data_out(load_data_out),
+        .load_data_out(load_data_out /*&& mem_stage_rdy_temp*/),//don't load until mem r/w finish
         .mdo_in(rs2_out),
-        .mdo_out(mem_wdata)
+        .mdo_out(mem_wdata_d)
     );
 
     mar mar_reg(
         .clk(clk),
         .reset(rst),
-        .load_mar(load_mar),
+        .load_mar(load_mar /*&& mem_stage_rdy_temp*/),//don't load until mem r/w finish
         .mar_in(marmux_o),
-        .mar_out(mem_address)
+        .mar_out(mem_address_d)
     );
 
 endmodule : mem_stage
