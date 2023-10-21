817fea8db8868d8d7d424f4f734bf91e2d3c5859
bak72019 <143014417+bak72019@users.noreply.github.com>
2023-10-20 22:07:00 -0500

    Add files via upload

--- mp4/hdl/exe_mem_reg.sv
+++ mp4/hdl/exe_mem_reg.sv
@@ -5,8 +5,9 @@ import cpuIO::*;
 (
     input clk, //from datapath
     input rst, //from datapath
-    input logic br_en_i //from exe_stage
-    input logic [31:0] alu_out //from exe_stage
+    input logic br_en_i, //from exe_stage
+    // input logic mem_stage_rdy, //from mem_stage
+    input logic [31:0] alu_out, //from exe_stage
     input logic [31:0] exe_pc_x, //from DE_EXE pipeline reg
     input logic [31:0] rs2_out_i, //from exe_stage
     input logic [31:0] u_imm_i, //from DE_EXE pipeline reg
@@ -19,16 +20,28 @@ import cpuIO::*;
     output logic [31:0] mem_pc_x, //to MEM_WB pipeline reg
     output logic [31:0] rs2_out_o, //to mem_stage
     output logic [31:0] u_imm_o, //to MEM_WB pipeline reg
-    output rv32i_opcode opcode_o, //to MEM_WB pipeline reg
+    output rv32i_opcode opcode_o, //to MEM_WB pipeline reg / mem_stage
     output logic br_en_o //to ctrl??? / MEM_WB pipeline reg
 );
+    logic [31:0] fwd_r_EX, pc_x_r, rs2_out_r, u_imm_r;
+    rv32i_opcode opcode_r;
+    cw_mem ctrl_w_r_MEM;
+    cw_writeback ctrl_w_r_WB;
+    logic br_en_r;
+
     //serves as alu_out reg/fwding exe data reg for cp2 onward
     always_ff @ (posedge clk, posedge rst) begin : fwd_EX_reg
         if(rst)begin
             exe_fwd_data <= 32'b0;
+            // fwd_r_EX <= 32'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     exe_fwd_data <= alu_out;
+        //     fwd_r_EX <= alu_out;
+        // end
         else begin
-            exe_fwd_data <= alu_out;
+            // exe_fwd_data <= fwd_r_EX;
+            exe_fwd_data <= alu_out; //erase when implement stage ready system
         end
     end
 
@@ -36,9 +49,15 @@ import cpuIO::*;
     always_ff @ (posedge clk, posedge rst) begin : pc_x_register
         if(rst)begin
             mem_pc_x <= 32'h40000000;
+            // pc_x_r <= 32'h40000000;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     mem_pc_x <= exe_pc_x;
+        //     pc_x_r <= exe_pc_x;
+        // end
         else begin
-            mem_pc_x <= exe_pc_x;
+            // mem_pc_x <= pc_x_r;
+            mem_pc_x <= exe_pc_x; //erase when implement stage ready system
         end
     end
 
@@ -46,19 +65,31 @@ import cpuIO::*;
     always_ff @ (posedge clk, posedge rst) begin : rs2_out_register
         if(rst)begin
             rs2_out_o <= 32'b0;
+            // rs2_out_r <= 32'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     rs2_out_o <= rs2_out_i;
+        //     rs2_out_r <= rs2_out_i;
+        // end
         else begin
-            rs2_out_o <= rs2_out_i;
+            // rs2_out_o <= rs2_out_r;
+            rs2_out_o <= rs2_out_i; //erase when implement stage ready system
         end
     end
 
     //control word for MEM register
     always_ff @ (posedge clk, posedge rst) begin : ctrl_w_MEM_register
         if(rst)begin
-            ctrl_w_MEM_o <= 32'b0;
+            ctrl_w_MEM_o <= 3'b0;
+            // ctrl_w_r_MEM <= 3'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     ctrl_w_MEM_o <= ctrl_w_MEM_i;
+        //     ctrl_w_r_MEM <= ctrl_w_MEM_i;
+        // end
         else begin
-            ctrl_w_MEM_o <= ctrl_w_MEM_i;
+            // ctrl_w_MEM_o <= ctrl_w_r_MEM;
+            ctrl_w_MEM_o <= ctrl_w_MEM_i; //erase when implement stage ready system
         end
     end
 
@@ -66,29 +97,47 @@ import cpuIO::*;
     always_ff @ (posedge clk, posedge rst) begin : ctrl_w_WB_register
         if(rst)begin
             ctrl_w_WB_o <= 32'b0;
+            // ctrl_w_r_WB <= 32'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     ctrl_w_WB_o <= ctrl_w_WB_i;
+        //     ctrl_w_r_WB <= ctrl_w_WB_i;
+        // end
         else begin
-            ctrl_w_WB_o <= ctrl_w_WB_i;
+            // ctrl_w_WB_o <= ctrl_w_r_WB;
+            ctrl_w_WB_o <= ctrl_w_WB_i; //erase when implement stage ready system
         end
     end
 
     //br_en register
     always_ff @ (posedge clk, posedge rst) begin : br_en_register
         if(rst)begin
-            br_en_o <= 32'b0;
+            br_en_o <= 1'b0;
+            // br_en_r <= 1'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     br_en_o <= br_en_i;
+        //     br_en_r <= br_en_i;
+        // end
         else begin
-            br_en_o <= br_en_i;
+            // br_en_o <= br_en_r;
+            br_en_o <= br_en_i; //erase when implement stage ready system
         end
     end
 
     //opcode register
     always_ff @ (posedge clk, posedge rst) begin : opcode_register
         if(rst)begin
-            opcode_o <= 32'b0;
+            opcode_o <= 7'b0;
+            // opcode_r <= 7'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     opcode_o <= opcode_i;
+        //     opcode_r <= opcode_i;
+        // end
         else begin
-            opcode_o <= opcode_i;
+            // opcode_o <= opcode_r;
+            opcode_o <= opcode_i; //erase when implement stage ready system
         end
     end
 
@@ -96,9 +145,15 @@ import cpuIO::*;
     always_ff @ (posedge clk, posedge rst) begin : u_imm_register
         if(rst)begin
             u_imm_o <= 32'b0;
+            // u_imm_r <= 32'b0;
         end
+        // else if(mem_stage_rdy == 1) begin
+        //     u_imm_o <= u_imm_i;
+        //     u_imm_r <= u_imm_i;
+        // end
         else begin
-            u_imm_o <= u_imm_i;
+            // u_imm_o <= u_imm_r;
+            u_imm_o <= u_imm_i; //erase when implement stage ready system
         end
     end
 
