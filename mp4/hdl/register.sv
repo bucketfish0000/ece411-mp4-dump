8c353fac8c5c4a4f70b1bb38da9e9bbc41632f9c
ethan chow <ethanc6@illinois.edu>
2023-10-23 15:12:19 -0500

    register add width =32 resetdata=0 parameters. shouldnt break anythign

--- mp4/hdl/register.sv
+++ mp4/hdl/register.sv
@@ -1,24 +1,33 @@
-module register
+module register #(parameter width = 32, 
+                  parameter resetData = 32'h00000000)
 (
     input clk,
     input rst,
     input load,
-    input [31:0] in,
-    output logic [31:0] out
+    input [width - 1:0] in,
+    //input [4:0] src_a, src_b, dest,
+    output logic [width - 1:0] out
 );
 
-logic [31:0] data = '0;
+//logic [31:0] data [32] /* synthesis ramstyle = "logic" */ = '{default:'0};
+logic [width - 1:0] data; 
 
 always_ff @(posedge clk)
 begin
-    if (rst) data <= '0;
-    else if (load) data <= in;
-    else data <= data;
+    if (rst)
+    begin
+        data <= resetData;
+    end
+    else if (load)
+    begin
+        data <= in;
+        //data <= 32'h00000000;
+    end
 end
 
 always_comb
 begin
-    out = data;
+    out <= data; 
 end
 
 endmodule : register
