659132a49f15b34068d7d77264f6dc49f7a66088
tongyul3 <tongyul3@linux-ssh-01.ews.illinois.edu>
2023-11-13 22:17:33 -0600

    jump should be working

--- mp4/testcode/competition/jump.s
+++ mp4/testcode/competition/jump.s
@@ -5,17 +5,21 @@ jump.s:
     # Refer to the RISC-V ISA Spec for the functionality of
     # the instructions in this test program.
 _start:
-    addi x3, x0, 17 #x3 <= 0x11
-    jal x1, loop
-    ori x2, x3, 32  #x2 <= 0x31
-    jal x0, halt
+    addi x3, x0, 17 #x3 <= 0x11 #0
+    jal x1, loop #4
+    nop #8
+    nop #c
+    nop #10
+    nop #14
+    ori x2, x3, 32  #x2 <= 0x31 #18
+    jal x0, halt #1c
 
 loop:
-    andi x4, x3, 33  #x4 <= 0x01
-    ret
+    andi x4, x3, 33  #x4 <= 0x01 #20
+    ret #24
 
 halt:                 # Infinite loop to keep the processor
-    beq x0, x0, halt  # from trying to execute the data below.
+    beq x0, x0, halt  #28 # from trying to execute the data below.
                       # Your own programs should also make use
                       # of an infinite loop at the end.
 
