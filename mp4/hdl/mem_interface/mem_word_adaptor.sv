659876b5b374f3bfebd2c1cc7e2e2a47a673908a
bfish <piscibussitula@gmail.com>
2023-11-16 19:49:19 -0600

    fixed shadow pc problem (1)

--- mp4/hdl/mem_interface/mem_word_adaptor.sv
+++ mp4/hdl/mem_interface/mem_word_adaptor.sv
@@ -22,7 +22,7 @@ logic [2:0] word_offset;
 assign word_offset = mem_address[4:2]; // 0 to 7 index word within cacheline 
 assign cacheline_wdata = {8{mem_wdata}}; 
 assign mem_rdata = cacheline_rdata[(32*word_offset) +: 32]; 
-assign cacheline_mem_byte_enable = {28'b0, mem_byte_enable} << (word_offset << 2); 
-assign cacheline_mem_address = {mem_address[31:8],8'b00000000};
+assign cacheline_mem_byte_enable = {28'h0, mem_byte_enable} << (mem_address[4:2]*4);
+assign cacheline_mem_address = {mem_address[31:5],5'b0};
  
 endmodule : mem_word_adapter
