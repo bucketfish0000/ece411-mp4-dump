d11494848979fb1f437d839e5cef3ce041f90f22
tongyul3 <tongyul3@linux-ssh-08.ews.illinois.edu>
2023-11-25 18:01:40 -0600

    fiexed jmp fwd problem

--- mp4/pkg/types.sv
+++ mp4/pkg/types.sv
@@ -84,9 +84,10 @@ typedef enum bit [1:0] {
 endpackage
 
 package memfwdmux;
-typedef enum bit { 
-    mem_fwd_data = 1'b0
-    ,exe_fwd_data = 1'b1
+typedef enum bit[1:0] { 
+    mem_fwd_data = 2'b00
+    ,exe_fwd_data = 2'b01
+    ,pc_plus_4 = 2'b10
 } memfwdmux_sel_t;
 endpackage
 
