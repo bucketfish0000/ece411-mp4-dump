04cd11e68fd97ed126de3ce172cf88cb0607d9d0
tongyul3 <tongyul3@linux-ssh-09.ews.illinois.edu>
2023-12-03 17:36:18 -0600

    aaaa

--- mp4/hdl/ctrl_buffer/prefetch_buffer.sv
+++ mp4/hdl/ctrl_buffer/prefetch_buffer.sv
@@ -21,35 +21,36 @@ module prefetch_buffer(
     input logic [255:0] mem_rdata, //data read from pmem
     output logic pf_hit, //pf_hits
     output logic pf_miss, //pf_misses
-    output logic pf_resp, //pf_resp
     output logic [255:0] pf_mem_rdata
 );
-    logic least_recent_used;
-    logic used;
+    logic volatile;
+    logic hit_num;
     logic [31:0] addrs [1:0];
     logic [255:0] cache_lines [1:0];
     logic hit, miss;
 
     assign pf_hit = hit;
     assign pf_miss = miss;
-    assign pf_mem_rdata = cache_lines[used];
+    assign pf_mem_rdata = cache_lines[hit_num];
     
     always_ff @( posedge clk, posedge rst ) begin
         if(rst) begin
-            least_recent_used <= 1'b0;
-            addrs[1:0] <= 64'b0;
-            cache_lines[1:0] <= 512'b0;
+            volatile <= 1'b0;
+            addrs[0] <= 32'b0;
+            addrs[1] <= 32'b0;
+            cache_lines[0] <= 256'b0;
+            cache_lines[1] <= 256'b0;
         end
         else begin
             if(miss&&pf_write) begin
                 if(pf_ld) begin
-                    addrs[least_recent_used] <= mem_addr;
-                    cache_lines[least_recent_used] <= mem_rdata;
-                    least_recent_used <= !least_recent_used;
+                    addrs[volatile] <= mem_addr;
+                    cache_lines[volatile] <= mem_rdata;
+                    volatile <= !volatile;
                 end
             end
             else if(hit&&(pf_write||pf_read)) begin
-                least_recent_used <= !used;
+                volatile <= !hit_num;
             end
             else begin
                 ;
@@ -60,32 +61,18 @@ module prefetch_buffer(
     always_comb begin
         hit = 1'b0;
         miss = 1'b0;
-        used = 1'b0;
-        pf_resp = 1'b0;
+        hit_num = 1'b0;
 
-        if((pf_write || pf_read)) begin
-            if((mem_addr == addrs[0])) begin
-                hit = 1'b1;
-                pf_resp = 1'b1;
-                used = 1'b0;
-            end
-            else if((mem_addr == addrs[1])) begin
-                hit = 1'b1;
-                pf_resp = 1'b1;
-                used = 1'b1;
-            end
-            else begin
-                miss = 1'b1;
-                if(pf_ld) begin
-                    pf_resp = 1'b1;
-                end
-                else begin
-                    pf_resp = 1'b0;
-                end
-            end
+        if((mem_addr == addrs[0])) begin
+            hit = 1'b1;
+            hit_num = 1'b0;
+        end
+        else if((mem_addr == addrs[1])) begin
+            hit = 1'b1;
+            hit_num = 1'b1;
         end
         else begin
-            //do nothing
+            miss = 1'b1;
         end
 
     end
