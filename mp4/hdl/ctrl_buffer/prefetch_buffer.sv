453a4c1c02782242129ca5cf407c6b72a2dcfbeb
Benjamin Kalnbach <bak7@illinois.edu>
2023-12-03 15:41:20 -0600

    stuff

--- mp4/hdl/ctrl_buffer/prefetch_buffer.sv
+++ mp4/hdl/ctrl_buffer/prefetch_buffer.sv
@@ -2,13 +2,9 @@
     General Idea:
         If imem is reading and is going to miss, but pf_buff has the cacheline it needs, grab from prefetch and write it into cache
 
-        If imem is writing and is going to miss, but pf_buff has the cacheline it needs, grab from prefetch and write to cache, then overwrite
-        THEN pf_buff needs to get rid of that cacheline since it is not up to date
+        If imem is reading and going to miss, but pf_buff doesn't have it, imem read from pmem as normal
 
-        If imem is writing or reading and going to miss, but pf_buff doesn't have it, imem read from pmem as normal
-
-        If imem is writing or reading and going to hit, ignor pf_buff entirely, BUT if writing and hit and hit in pf_buff, then pf_buff needs
-        to get rid of the cacheline since it isn't up to date
+        If imem is reading and going to hit, ignore pf_buff entirely
 
         If imem and dmem are not using pmem, then pf_write high and petition pmem IF miss, then when pf_ld(==pmem_resp) update the buffer. However,
         if hit then don't petition pmem and change least_recently_used accordingly
@@ -19,26 +15,23 @@ module prefetch_buffer(
     input clk,
     input rst,
     input logic [31:0] mem_addr, //mem_addr, either imem_addr or new pf_addr
-    input logic imem_writing,
-    input logic pf_read, //imem is reading
+    input logic pf_read, //imem is reading, check for data
     input logic pf_write, //want to write something into pf_buf
     input logic pf_ld, //pmem_resp goes high, load cacheline
-    input logic [255:0] mem_rdata,
-    output logic pf_hit,
-    output logic pf_miss,
-    output logic pf_mem_resp,
-    output logic [255:0] pf_mem_rdata,
+    input logic [255:0] mem_rdata, //data read from pmem
+    output logic pf_hit, //pf_hits
+    output logic pf_miss, //pf_misses
+    output logic pf_resp, //pf_resp
+    output logic [255:0] pf_mem_rdata
 );
-
-    assign pf_hit = hit;
-    assign pf_miss = miss;
-
     logic least_recent_used;
     logic used;
     logic [31:0] addrs [1:0];
     logic [255:0] cache_lines [1:0];
     logic hit, miss, pf_miss_resp;
 
+    assign pf_hit = hit;
+    assign pf_miss = miss;
     assign pf_mem_rdata = cache_lines[used];
     
     always_ff @( posedge clk, posedge rst ) begin
@@ -56,20 +49,14 @@ module prefetch_buffer(
                     least_recent_used <= !least_recent_used;
                     pf_miss_resp <= 1'b1;
                 end
-                else pf_miss <= 1'b0;
+                else pf_miss_resp <= 1'b0;
             end
             else if(hit&&(pf_write||pf_read)) begin
                 least_recent_used <= !used;
                 pf_miss_resp <= 1'b0;
             end
-            else if(hit&&imem_writing) begin //hit and give imem the cacheline if it needs it, but then remove cacheline, it's dirty now
-                least_recent_used <= used;
-                pf_miss_resp <= 1'b0;
-                addrs[least_recent_used] <= 32'b0;
-                cache_lines[least_recent_used] <= 256'b0;
-            end
             else begin
-                pf_miss_resp == 1'b0;
+                pf_miss_resp <= 1'b0;
             end
         end
     end
@@ -80,7 +67,7 @@ module prefetch_buffer(
         used = 1'b0;
         pf_resp = 1'b0;
 
-        if((pf_write || pf_read || )) begin
+        if((pf_write || pf_read)) begin
             if((mem_addr == addrs[0])) begin
                 hit = 1'b1;
                 pf_resp = 1'b1;
@@ -93,7 +80,7 @@ module prefetch_buffer(
             end
             else begin
                 miss = 1'b1;
-                if(pf_mem_resp) begin
+                if(pf_miss_resp) begin
                     pf_resp = 1'b1;
                 end
                 else begin
